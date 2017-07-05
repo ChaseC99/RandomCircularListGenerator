bc867f53151506d33c682e4d9fcfbc56123f7182
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-07-04 17:08:38 -0700

    Update Comments
    Updated header block comments and rearanged methods

--- Person.java
+++ Person.java
@@ -1,11 +1,24 @@
 /** RandomCircularListGenerator
  *  @author Chase Carnaroli
  *
- *  Each person on the
+ *  Each person is represented by a Person class
+ *  The Person class tracks each person's name, team, and target
  *
+ *  INSTANCE VARIABLES
+ *      String name         // person's name
+ *      Team team           // the team that the person belongs to
+ *      Person target       // the person that comes after this person on the list
  *
+ *  METHODS
+ *      getName() -> String               // returns person's name
+ *      getTeam() -> Team                 // returns person's team
+ *      setTarget()                       // sets person's target
+ *      getTarget() -> Person             // returns person's target
+ *      diffTeams(Person) -> boolean      // returns true if different teams, false if same team
+ *      diffTeams(Team) -> boolean        // returns true if different teams, false if same team
+ *      equals(Person) -> boolean         // return true if same person, false if different
  */
-public class Player
+public class Person
 {
     // Instance Variables
     private String name;
@@ -21,19 +34,14 @@ public class Player
         this.team = team;
     }
 
+    // post: return name
+    public String getName(){
+        return name;
+    }
+
     // post: returns team
     public Team getTeam(){
         return team;
-    }
-
-    // post: returns true if different teams, false if same team
-    public boolean diffTeams(Person person){
-        return team.equals(person.getTeam());
-    }
-
-    // post: returns true if different teams, false if same team
-    public boolean diffTeams(Team teamName){
-        return team.equals(teamName);
     }
 
     // post: set target
@@ -46,9 +54,14 @@ public class Player
         return target;
     }
 
-    // post: return name
-    public String getName(){
-        return name;
+    // post: returns true if different teams, false if same team
+    public boolean diffTeams(Person person){
+        return team.equals(person.getTeam());
+    }
+
+    // post: returns true if different teams, false if same team
+    public boolean diffTeams(Team teamName){
+        return team.equals(teamName);
     }
 
     // post: return true if same object, false if different
