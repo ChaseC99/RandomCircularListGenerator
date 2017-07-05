7ca314ec8791bc7561e48824ed4e6f14a8681293
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-07-05 00:55:40 -0700

    Create Team Class
    Changed team from an enum to a class. Now more or less teams can be inputed, depending on what the user wants.
    
    Will need to change how RandomCircularList.java works because this breaks it.

--- Team.java
+++ Team.java
@@ -1,11 +1,53 @@
 
-/**
- * Enumeration class Team - write a description of the enum class here
- * 
- * @author (your name here)
- * @version (version number or date here)
+/** RandomCircularListGenerator
+ *  @author Chase Carnaroli
+ *  Enumeration class Team - write a description of the enum class here
+ *
+ *
  */
-public enum Team
+
+ import java.util.*;    // import arraylists
+public class Team
 {
-    NOVICE, JV, VARSITY, STATGIRLS
+    // Instance Variables
+    private String teamName;
+    private List<Person> people;
+
+    // Constructors
+    public Team(String teamName){
+        this.teamName = teamName;
+        people = new ArrayList<People>();
+    }
+
+    public Team(String teamName, List<Person> people){
+        this.teamName = teamName;
+        this.people = people;
+    }
+
+    // post: returns number of people on the team
+    public int size(){
+      return people.size();
+    }
+
+    public void add(Person person){
+        people.add(person);
+    }
+
+    public boolean remove(Person person){
+        int index = people.indexOf(person);
+        if(index != -1){
+            people.remove(index);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setPeople(List<People> people){
+        this.people = people;
+    }
+
+    public List<Person> getPeople(){
+        return people;
+    }
 }
