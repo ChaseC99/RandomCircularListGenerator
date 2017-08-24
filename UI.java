80b6db42b1e482664747501df8e4d0cdf26c1181
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-08-24 13:42:52 -0700

    Add UI file

---
+++ UI.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class UI extends JFrame
+{
+    // constructor
+    public UI() {
+        super("Random List Generator");
+
+        // JPanels
+        JPanel playerView = new JPanel();
+        add(playerView);
+
+        add(new JSeparator(JSeparator.VERTICAL), BorderLayout.LINE_START);
+
+        JPanel listView = new JPanel();
+        add(listView);
+
+        // DON'T FORGET TO INCLUDE THIS CODE - otherwise you will not
+        // be able to close your application!!!
+        addWindowListener(new java.awt.event.WindowAdapter()
+            {
+                public void windowClosing(WindowEvent evt) {
+                    System.exit(0);
+                }
+            }
+        );
+
+        // Set window size and show window
+        Dimension screenSize = new Dimension(700,500);
+        setMinimumSize(screenSize);     // width=500, height=400
+        setVisible(true);
+    }
+}
