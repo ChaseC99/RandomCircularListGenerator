f9886dfb8d2bf12deb29337b50a30282699ac9cf
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-08-26 01:25:54 -0700

    Create ListViewType Represents the different possible list display view types

---
+++ ListViewType.java
@@ -0,0 +1,9 @@
+/** RandomCircularListGenerator
+ *  @author Chase Carnaroli
+ *
+ *  Enum to represent the different possible view types for list display
+ */
+public enum ListViewType
+{
+    LIST, SINGLE_NEXT
+}
