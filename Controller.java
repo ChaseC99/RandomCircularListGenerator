1ff27bf9d58dea83aca1e15682ecc343cf4071b8
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-08-25 19:54:30 -0700

    Create Controller Class

---
+++ Controller.java
@@ -0,0 +1,17 @@
+/** RandomCircularListGenerator
+ *  @author Chase Carnaroli
+ *
+ *  This is the controller, which runs the whole program.
+ */
+
+public class Controller
+{
+    // Instance Variables
+    Window ui;
+
+    // Constructor
+    public Controller(Window ui)
+    {
+        this.ui = ui;
+    }
+}
