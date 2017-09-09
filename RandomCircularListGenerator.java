7e1c5c884168213a1d1bf8214c2b76f6e1f64281
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-09-09 13:41:29 -0700

    Set look and feel

--- RandomCircularListGenerator.java
+++ RandomCircularListGenerator.java
@@ -4,6 +4,9 @@
  *  This is the main, which sets up the whole program.
  */
 
+ import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class RandomCircularListGenerator
 {
     // Instance Variables
@@ -13,8 +16,30 @@ public class RandomCircularListGenerator
     // Constructor
     public static void main(String[] args)
     {
-        ui = new Window();
-        controller = new Controller(ui);
-        ui.setController(controller);
+        // Set Look and Feel of Program
+        try {
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName());
+        }
+        catch (UnsupportedLookAndFeelException e) {
+            // handle exception
+        }
+        catch (ClassNotFoundException e) {
+            // handle exception
+        }
+        catch (InstantiationException e) {
+            // handle exception
+        }
+        catch (IllegalAccessException e) {
+            // handle exception
+        }
+
+
+
+        // Start the program up
+        ui = new Window();                  // Create Window
+        controller = new Controller(ui);    // Create controller
+        ui.setController(controller);       // Set window's controller
     }
 }
